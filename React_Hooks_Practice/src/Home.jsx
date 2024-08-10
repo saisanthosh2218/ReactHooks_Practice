58878ef4742ffd0bcbb309b415b72cde914bb84e
saisanthosh2218 <saisanthoshbunni@gmail.com>
2024-08-10 20:51:03 +0530

    added practice hooks

---
+++ React_Hooks_Practice/src/Home.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="links">
+        <Link to={"/"}>Home Page</Link>
+        <Link to={"/callback"}>Call Back</Link>
+        <Link to={"/memo"}>Memo</Link>
+        <Link to={"/lazyload"}>LazyLoad</Link>
+        <Link to={"/reference"}>Reference</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ React_Hooks_Practice/src/LazyLoad.jsx
@@ -0,0 +1,5 @@
+function LazyLoad() {
+  return <div></div>;
+}
+
+export default LazyLoad;
